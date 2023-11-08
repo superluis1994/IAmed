46bb1c154f3b414f4b6d85f313adad3ecf40c3a3
Paul Cartagena <garycartagena04@gmail.com>
2023-11-08 16:53:30 -0600

    Mis cambios

--- resources/views/dashboard/chatbot.php
+++ resources/views/dashboard/chatbot.php
@@ -138,7 +138,11 @@
               <div class="col-12 col-md-6 order-md-1 order-last">
                 <h3>MediBot</h3>
                 <p class="text-subtitle text-muted">
+<<<<<<< HEAD
                   Conversación
+=======
+                  Converzación
+>>>>>>> ff8152c (Mis cambios)
                 </p>
               </div>
               <div class="col-12 col-md-6 order-md-2 order-first">
