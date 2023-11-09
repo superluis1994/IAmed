d9b05c0c16e43200e4db742f8fb88ee50e4bd394
Paul Cartagena <garycartagena04@gmail.com>
2023-11-08 21:40:02 -0600

    Modificaciones durante reunion

--- resources/views/dashboard/chatMedico.php
+++ resources/views/dashboard/chatMedico.php
@@ -1,38 +1,84 @@
 <div id="app">
-  <div id="sidebar">
-    <div class="sidebar-wrapper active">
-      <div class="sidebar-header position-relative">
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="logo">
-            <a href="index.html"><img src="<?= $utils->assets('assets/compiled/svg/logo.svg') ?>" alt="Logo" srcset="" /></a>
-          </div>
-          <div class="theme-toggle d-flex gap-2 align-items-center mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--system-uicons" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 21 21">
-              <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M10.5 14.5c2.219 0 4-1.763 4-3.982a4.003 4.003 0 0 0-4-4.018c-2.219 0-4 1.781-4 4c0 2.219 1.781 4 4 4zM4.136 4.136L5.55 5.55m9.9 9.9l1.414 1.414M1.5 10.5h2m14 0h2M4.135 16.863L5.55 15.45m9.899-9.9l1.414-1.415M10.5 19.5v-2m0-14v-2" opacity=".3"></path>
-                <g transform="translate(-210 -1)">
-                  <path d="M220.5 2.5v2m6.5.5l-1.5 1.5"></path>
-                  <circle cx="220.5" cy="11.5" r="4"></circle>
-                  <path d="m214 5l1.5 1.5m5 14v-2m6.5-.5l-1.5-1.5M214 18l1.5-1.5m-4-5h2m14 0h2"></path>
-                </g>
-              </g>
-            </svg>
-            <div class="form-check form-switch fs-6">
-              <input class="form-check-input me-0" type="checkbox" id="toggle-dark" style="cursor: pointer" />
-              <label class="form-check-label"></label>
+      <div id="sidebar">
+        <div class="sidebar-wrapper active">
+          <div class="sidebar-header position-relative">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="logo">
+                <a href="index.html"
+                  ><img
+                    src="<?= $utils->assets('assets/compiled/svg/logo.svg') ?>"
+                    alt="Logo"
+                    srcset=""
+                /></a>
+              </div>
+              <div class="theme-toggle d-flex gap-2 align-items-center mt-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  aria-hidden="true"
+                  role="img"
+                  class="iconify iconify--system-uicons"
+                  width="20"
+                  height="20"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 21 21"
+                >
+                  <g
+                    fill="none"
+                    fill-rule="evenodd"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path
+                      d="M10.5 14.5c2.219 0 4-1.763 4-3.982a4.003 4.003 0 0 0-4-4.018c-2.219 0-4 1.781-4 4c0 2.219 1.781 4 4 4zM4.136 4.136L5.55 5.55m9.9 9.9l1.414 1.414M1.5 10.5h2m14 0h2M4.135 16.863L5.55 15.45m9.899-9.9l1.414-1.415M10.5 19.5v-2m0-14v-2"
+                      opacity=".3"
+                    ></path>
+                    <g transform="translate(-210 -1)">
+                      <path d="M220.5 2.5v2m6.5.5l-1.5 1.5"></path>
+                      <circle cx="220.5" cy="11.5" r="4"></circle>
+                      <path
+                        d="m214 5l1.5 1.5m5 14v-2m6.5-.5l-1.5-1.5M214 18l1.5-1.5m-4-5h2m14 0h2"
+                      ></path>
+                    </g>
+                  </g>
+                </svg>
+                <div class="form-check form-switch fs-6">
+                  <input
+                    class="form-check-input me-0"
+                    type="checkbox"
+                    id="toggle-dark"
+                    style="cursor: pointer"
+                  />
+                  <label class="form-check-label"></label>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  aria-hidden="true"
+                  role="img"
+                  class="iconify iconify--mdi"
+                  width="20"
+                  height="20"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="m17.75 4.09l-2.53 1.94l.91 3.06l-2.63-1.81l-2.63 1.81l.91-3.06l-2.53-1.94L12.44 4l1.06-3l1.06 3l3.19.09m3.5 6.91l-1.64 1.25l.59 1.98l-1.7-1.17l-1.7 1.17l.59-1.98L15.75 11l2.06-.05L18.5 9l.69 1.95l2.06.05m-2.28 4.95c.83-.08 1.72 1.1 1.19 1.85c-.32.45-.66.87-1.08 1.27C15.17 23 8.84 23 4.94 19.07c-3.91-3.9-3.91-10.24 0-14.14c.4-.4.82-.76 1.27-1.08c.75-.53 1.93.36 1.85 1.19c-.27 2.86.69 5.83 2.89 8.02a9.96 9.96 0 0 0 8.02 2.89m-1.64 2.02a12.08 12.08 0 0 1-7.8-3.47c-2.17-2.19-3.33-5-3.49-7.82c-2.81 3.14-2.7 7.96.31 10.98c3.02 3.01 7.84 3.12 10.98.31Z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="sidebar-toggler x">
+                <a href="#" class="sidebar-hide d-xl-none d-block"
+                  ><i class="bi bi-x bi-middle"></i
+                ></a>
+              </div>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--mdi" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-              <path fill="currentColor" d="m17.75 4.09l-2.53 1.94l.91 3.06l-2.63-1.81l-2.63 1.81l.91-3.06l-2.53-1.94L12.44 4l1.06-3l1.06 3l3.19.09m3.5 6.91l-1.64 1.25l.59 1.98l-1.7-1.17l-1.7 1.17l.59-1.98L15.75 11l2.06-.05L18.5 9l.69 1.95l2.06.05m-2.28 4.95c.83-.08 1.72 1.1 1.19 1.85c-.32.45-.66.87-1.08 1.27C15.17 23 8.84 23 4.94 19.07c-3.91-3.9-3.91-10.24 0-14.14c.4-.4.82-.76 1.27-1.08c.75-.53 1.93.36 1.85 1.19c-.27 2.86.69 5.83 2.89 8.02a9.96 9.96 0 0 0 8.02 2.89m-1.64 2.02a12.08 12.08 0 0 1-7.8-3.47c-2.17-2.19-3.33-5-3.49-7.82c-2.81 3.14-2.7 7.96.31 10.98c3.02 3.01 7.84 3.12 10.98.31Z"></path>
-            </svg>
           </div>
-          <div class="sidebar-toggler x">
-            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
-          </div>
-        </div>
-      </div>
-      <div class="sidebar-menu">
-        <ul class="menu">
-          <li class="sidebar-title">Menu</li>
+          <div class="sidebar-menu">
+            <ul class="menu">
+              <li class="sidebar-title">Menú</li>
 
           <li class="sidebar-item">
             <a href="<?= $utils->Url("/dashboard"); ?>" class="sidebar-link">
@@ -75,8 +121,172 @@
       </a>
     </header>
 
+        <div class="page-heading">
+          <div class="page-title">
+            <div class="row">
+              <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Médico</h3>
+                <p class="text-subtitle text-muted">
+                  Conversación
+                </p>
+              </div>
+              <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav
+                  aria-label="breadcrumb"
+                  class="breadcrumb-header float-start float-lg-end"
+                >
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                      <a href="<?=$utils->Url("/dashboard");?>">Dashboard</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">
+                      ChatIA
+                    </li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <section class="section">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-header">
+                    <div class="media d-flex align-items-center">
+                      <div class="avatar me-3">
+                        <img
+                          src="./assets/compiled/jpg/1.jpg"
+                          alt=""
+                          srcset=""
+                        />
+                        <span class="avatar-status bg-success"></span>
+                      </div>
+                      <div class="name flex-grow-1">
+                        <h6 class="mb-0">MediBot</h6>
+                        <span class="text-xs"> Siempre en linea</span>
+                      </div>
+                      <button class="btn btn-sm">
+                        <i data-feather="x"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body pt-4 bg-grey">
+                    <div class="chat-content">
+                      <div class="chat">
+                        <div class="chat-body">
+                          <div class="chat-message">
+                          Hola, tengo estos sintomas : Disnea,  Sibilancias, Tos, y Opresión en el pecho.
+                          </div>
+                        </div>
+                      </div>
+                      <div class="chat chat-left">
+                        <div class="chat-body">
+                          <div class="chat-message">
+                          "Basado en los síntomas que has proporcionado, es posible que estés experimentando asma. El asma es una afección crónica que afecta las vías respiratorias y puede manifestarse con síntomas como disnea (dificultad para respirar), sibilancias, tos y opresión en el pecho. Es importante que busques atención médica para obtener un diagnóstico preciso y un plan de tratamiento adecuado.
+                          </div>
+                          <div class="chat-message">
+                          Te recomendamos estos especialistas de IAmed:
+                          </div>
+                        </div>
+                      </div>
+                      <style>
+    .doctor-info {
+      background-color: #007BFF; 
+      color: #fff; 
+      padding: 10px; 
+      border-radius: 5px; 
+      margin-bottom: 10px; 
+    }
 
+    .doctor-name {
+      font-weight: bold; 
+    }
 
-  
-  <!-- </div>
-</div> -->
+    .doctor-circle {
+      width: 50px; 
+      height: 50px;
+      background-color: #ccc; 
+      border-radius: 50%; 
+      display: inline-block; 
+      margin-right: 10px; 
+    }
+  </style>
+</head>
+<body>
+  <div class="chat chat-left">
+    <div class="chat-body">
+      <div class="chat-message">
+        <div class="doctor-info">
+          <div class="doctor-circle"></div>
+          <p><a href="enlace-al-dr-luis-respirino" class="doctor-name">Dr. Luis Respirino</a></p>
+          <p>Especialidad: Neumonología y Alergología.</p>
+        </div>
+        <div class="doctor-info">
+          <div class="doctor-circle"></div>
+          <p><a href="enlace-a-la-dra-maria-fernanda" class="doctor-name">Dra. Maria Fernanda</a></p>
+          <p>Especialidad: Pediatría y Alergología.</p>
+        </div>
+        <div class="doctor-info">
+          <div class="doctor-circle"></div>
+          <p><a href="enlace-al-dr-oscar-zapata" class="doctor-name">Dr. Oscar Zapata</a></p>
+          <p>Especialidad: Neumonología y Medicina Interna.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+                      <div class="chat">
+                        <div class="chat-body">
+                          <div class="chat-message">
+                            OK, muchas gracias mediBot
+                          </div>
+                        </div>
+                      </div>
+                      <div class="chat chat-left">
+                        <div class="chat-body">
+                          <div class="chat-message">
+                          Estoy para ayudarte, estare aqui si necesitas más ayuda!
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-footer">
+                    <div
+                      class="message-form d-flex flex-direction-column align-items-center"
+                    >
+                      <a href="http://" class="black"
+                        ><i data-feather="smile"></i
+                      ></a>
+                      <div class="d-flex flex-grow-1 ms-4">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Type your message.."
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <footer>
+          <div class="footer clearfix mb-0 text-muted">
+            <div class="float-start">
+              <p>2023 &copy; Mazer</p>
+            </div>
+            <div class="float-end">
+             <!-- <p>
+                Crafted with
+                <span class="text-danger"
+                  ><i class="bi bi-heart-fill icon-mid"></i
+                ></span>
+                by <a href="https://saugi.me">Saugi</a> -->
+              </p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
