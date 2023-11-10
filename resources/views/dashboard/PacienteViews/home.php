9e9b5eaef06ce65cf48492664a47c03d68cbe93c
Paul Cartagena <garycartagena04@gmail.com>
2023-11-10 14:09:36 -0600

    He dejado el perfil del usuario como el del medico, he comentado la parte de publicidad y a el contenedor principal ahora tiene un md-12

--- resources/views/dashboard/PacienteViews/home.php
+++ resources/views/dashboard/PacienteViews/home.php
@@ -1,19 +1,19 @@
 
+<div class="col-12 col-lg-12">
 <div class="col-12 col-lg-11">
   <div class="row">
     <div class="col-6 col-lg-3 col-md-6">
-      <a href="<?=$utils->url('/dashboard/comunidaMedica');?>">
-        <div class="card">
-          <div class="card-body px-4 py-5">
-            <div class="row">
-              <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                <div class="stats-icon purple mb-2">
-                  <i class="bi bi-people-fill"></i>
-                </div>
+      <div class="card">
+        <div class="card-body px-4 py-5">
+          <div class="row">
+            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+              <div class="stats-icon purple mb-2">
+                <i class="bi bi-people-fill"></i>
               </div>
-              <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                <h6 class="text-muted font-semibold">
-                comunidad
+            </div>
+            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+              <h6 class="text-muted font-semibold">
+                Comunidad
               </h6>
               <h6 class="font-extrabold mb-0">Médica</h6>
             </div>
@@ -96,7 +96,7 @@
                 <svg class="bi text-primary" width="32" height="32" fill="blue" style="width: 10px">
                   <use xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill" />
                 </svg>
-                <h5 class="mb-0 ms-3">Citas medicas</h5>
+                <h5 class="mb-0 ms-3">Citas Médicas</h5>
               </div>
             </div>
             <div class="col-5">
@@ -146,7 +146,7 @@
                       <div class="avatar avatar-md">
                         <img src="./assets/compiled/jpg/5.jpg" />
                       </div>
-                      <p class="font-bold ms-3 mb-0">Gary Paul Cartagena</p>
+                      <p class="font-bold ms-3 mb-0">Paul Cartagena</p>
                     </div>
                   </td>
                   <td class="col-auto">
@@ -195,61 +195,60 @@
           </div>
         </div>
       </div>
-      
     </div>
-      <div class="col-12 col-lg-3">
-        <!-- <div class="card">
-          <div class="card-body py-4 px-4">
-            <div class="d-flex align-items-center">
-              <div class="avatar avatar-xl">
-                <img src="<?=$utils->assets('assets/compiled/jpg/1.jpg')?>" alt="Face 1" />
-              </div>
-              <div class="ms-2 name">
-                <h7 class="font-bold"><?php echo $_SESSION["datosUser"]["user"]; ?></h7>
-                <h6 class="text-muted mb-0">Emanuel Temple</h6>
-              </div>
-            </div>
-          </div>
-        </div> -->
-        <div class="card">
-          <div class="card-header">
-            <h4>Publicidad</h4>
-          </div>
-          <div class="card-content pb-4">
-            <div class="recent-message d-flex px-4 py-3">
-              <div class="avatar avatar-lg">
-                <img src="<?=$utils->assets('assets/compiled/jpg/4.jpg')?>" />
-              </div>
-              <div class="name ms-4">
-                <h5 class="mb-1">Gary Paul Cartagena</h5>
-                <h6 class="text-muted mb-0">Pediatra</h6>
-              </div>
-            </div>
-            <div class="recent-message d-flex px-4 py-3">
-              <div class="avatar avatar-lg">
-                <img src="<?=$utils->assets('assets/compiled/jpg/5.jpg')?>" />
-              </div>
-              <div class="name ms-4">
-                <h5 class="mb-1">Melvin Marroquín</h5>
-                <h6 class="text-muted mb-0">Médico General</h6>
-              </div>
-            </div>
-            <div class="recent-message d-flex px-4 py-3">
-              <div class="avatar avatar-lg">
-                <img src="<?=$utils->assets('assets/compiled/jpg/1.jpg')?>" />
-              </div>
-              <div class="name ms-4">
-                <h5 class="mb-1">John Chayanne</h5>
-                <h6 class="text-muted mb-0">Enfermero</h6>
-              </div>
-            </div>
-            <div class="px-4">
-              <button class="btn btn-block btn-xl btn-outline-primary font-bold mt-3">
-                Ver todos
-              </button>
-            </div>
-          </div>
+  </div>
+</div>
+<!-- <div class="col-12 col-lg-3">
+  <div class="card">
+    <div class="card-body py-4 px-4">
+      <div class="d-flex align-items-center">
+        <div class="avatar avatar-xl">
+          <img src="<?=$utils->assets('assets/compiled/jpg/1.jpg')?>" alt="Face 1" />
+        </div>
+        <div class="ms-2 name">
+          <h7 class="font-bold"><?php echo $_SESSION["datosUser"]["user"]; ?></h7>
+          <h6 class="text-muted mb-0">Emmanuel</h6>
         </div>
       </div>
+    </div>
   </div>
+  <div class="card">
+    <div class="card-header">
+      <h4>Publicidad</h4>
+    </div>
+    <div class="card-content pb-4">
+      <div class="recent-message d-flex px-4 py-3">
+        <div class="avatar avatar-lg">
+          <img src="<?=$utils->assets('assets/compiled/jpg/4.jpg')?>" />
+        </div>
+        <div class="name ms-4">
+          <h5 class="mb-1">Paul Cartagena</h5>
+          <h6 class="text-muted mb-0">Pediatra</h6>
+        </div>
+      </div>
+      <div class="recent-message d-flex px-4 py-3">
+        <div class="avatar avatar-lg">
+          <img src="<?=$utils->assets('assets/compiled/jpg/5.jpg')?>" />
+        </div>
+        <div class="name ms-4">
+          <h5 class="mb-1">Melvin Marroquín</h5>
+          <h6 class="text-muted mb-0">Médico General</h6>
+        </div>
+      </div>
+      <div class="recent-message d-flex px-4 py-3">
+        <div class="avatar avatar-lg">
+          <img src="<?=$utils->assets('assets/compiled/jpg/1.jpg')?>" />
+        </div>
+        <div class="name ms-4">
+          <h5 class="mb-1">Luis Sorto</h5>
+          <h6 class="text-muted mb-0">Enfermero</h6>
+        </div>
+      </div>
+      <div class="px-4">
+        <button class="btn btn-block btn-xl btn-outline-primary font-bold mt-3">
+          Ver todos
+        </button>
+      </div>
+    </div>
+  </div> -->
 </div>
