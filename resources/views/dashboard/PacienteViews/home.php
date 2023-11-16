800bbec2bb7d207504fc1df83f061837aff3119b
superluis1994 <alberto.sorto20@itca.edu.sv>
2023-11-16 10:43:03 -0600

    publicidad en el panel de paciente

--- resources/views/dashboard/PacienteViews/home.php
+++ resources/views/dashboard/PacienteViews/home.php
@@ -8,7 +8,7 @@
             <div class="row">
               <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                 <div class="stats-icon purple mb-2">
-                  <i class="bi bi-people-fill"></i>
+                  <i class="iconly-boldShield-Done"></i>
                 </div>
               </div>
               <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
@@ -198,58 +198,50 @@
       
     </div>
       <div class="col-12 col-lg-3">
-        <!-- <div class="card">
-          <div class="card-body py-4 px-4">
-            <div class="d-flex align-items-center">
-              <div class="avatar avatar-xl">
-                <img src="<?=$utils->assets('assets/compiled/jpg/1.jpg')?>" alt="Face 1" />
-              </div>
-              <div class="ms-2 name">
-                <h7 class="font-bold"><?php echo $_SESSION["datosUser"]["user"]; ?></h7>
-                <h6 class="text-muted mb-0">Emanuel Temple</h6>
-              </div>
-            </div>
-          </div>
-        </div> -->
-        <div class="card">
-          <div class="card-header">
-            <h4>Publicidad</h4>
-          </div>
-          <div class="card-content pb-4">
-            <div class="recent-message d-flex px-4 py-3">
-              <div class="avatar avatar-lg">
-                <img src="<?=$utils->assets('assets/compiled/jpg/2.jpg')?>" />
-              </div>
-              <div class="name ms-4">
-                <h5 class="mb-1">Paul Cartagena</h5>
-                <h6 class="text-muted mb-0">Pediatra</h6>
-              </div>
-            </div>
-            <div class="recent-message d-flex px-4 py-3">
-              <div class="avatar avatar-lg">
-                <img src="<?=$utils->assets('assets/compiled/jpg/4.jpg')?>" />
-              </div>
-              <div class="name ms-4">
-                <h5 class="mb-1">Melvin Marroquín</h5>
-                <h6 class="text-muted mb-0">Médico General</h6>
-              </div>
-            </div>
-            <div class="recent-message d-flex px-4 py-3">
-              <div class="avatar avatar-lg">
-                <img src="<?=$utils->assets('assets/compiled/jpg/7.jpg')?>" />
-              </div>
-              <div class="name ms-4">
-                <h5 class="mb-1">Luis Sorto</h5>
-                <h6 class="text-muted mb-0">Enfermero</h6>
-              </div>
-            </div>
-            <div class="px-4">
-              <button class="btn btn-block btn-xl btn-outline-primary font-bold mt-3">
-                Ver todos
-              </button>
-            </div>
-          </div>
-        </div>
+
+<style>
+  .carousel-caption {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.8;
+  padding: 20px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+</style>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?=$utils->assets('assets/compiled/jpg/building.jpg');?>" class="d-block w-1000" alt="...">
+      <div class="carousel-caption">
+        <a href="#" class="btn btn-primary" style="position: absolute; bottom: 0; right: 0;">ir al link</a>
+      </div>
+      
+    </div>
+    <div class="carousel-item active">
+      <img src="<?=$utils->assets('assets/compiled/jpg/building.jpg');?>" class="d-block w-1000" alt="...">
+      <div class="carousel-caption">
+        <a href="#" class="btn btn-primary" style="position: absolute; bottom: 0; right: 0;">ir al link</a>
+      </div>
+      
+    </div>
+  
+
+  </div>
+</div>
+
+
+
+      
+        
+        
+        
+
+
+
       </div>
   </div>
 </div>
--- routes/1web.php
+++ routes/1web.php
@@ -11,21 +11,6 @@ Route::get('/servicios', "MainControllers@funciones");
 Route::get('/contacto', "MainControllers@contacto");
 Route::get('/sorto', "SortoControllers@sorto");
 
-// Route::group('/web', function () {
-//     Route::get('/id', function () {
-//         return Utils::view("index");
-//     });
-//     // Route::post('',"Ruta inicial POST");
-//     Route::get('/nombre/{name}', function ($route) {
-//         return Utils::view("producto.index", ["name" => $route["name"]]);
-//     });
-// });
-// Route::get('/dashboard', "DashboardControllers@index");
-// Route::post('/api/{id}',"MainController@show");
-// Route::post('/api/{id}', function ($route) {
-//     // echo $route['id'];
-//     return $route['id'];
-// });
 // Route::post('/api/{id}-{nombre}', "Ruta inicial GET");
 Route::post('/personas/{id}', "PersonaControllers@show");
 Route::post('/persona/{id}', "MainControllers@index");
@@ -38,10 +23,5 @@ Route::group('/producto', function () {
         return Utils::view("producto.index", ["name" => $route["name"]]);
     });
 });
-    // Route::get('/',function(){
-    //     return [
-    //         "id" => 1
-    //     ];
-    // });
-    // print_r(Route::getRoutes());
+
  
