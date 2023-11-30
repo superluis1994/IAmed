be6ef83c2115b8ce663bf07f30f0bb3d402da3e2
MelvinJr23 <melvinmmc2805@gmail.com>
2023-11-30 14:39:38 -0600

    Suscripcion paciente

--- resources/views/dashboard/PacienteViews/suscripciones.php
+++ resources/views/dashboard/PacienteViews/suscripciones.php
@@ -2,9 +2,9 @@
         <div class="page-title">
           <div class="row">
             <div class="col-12 col-md-6 order-md-1 order-last">
-              <h3>Pricing</h3>
+              <h3>Planes de Suscripción</h3>
               <p class="text-subtitle text-muted">
-                Present your plans to your users.
+                Se beneficiado con los siguientes planes.
               </p>
             </div>
             <div class="col-12 col-md-6 order-md-2 order-first">
@@ -14,7 +14,7 @@
                     <a href="index.html">Dashboard</a>
                   </li>
                   <li class="breadcrumb-item active" aria-current="page">
-                    Pricing
+                    Planes de Suscripción
                   </li>
                 </ol>
               </nav>
@@ -30,34 +30,30 @@
                   <div class="col-md-4 px-0">
                     <div class="card">
                       <div class="card-header text-center">
-                        <h4 class="card-title">Basic</h4>
+                        <h4 class="card-title">Basica</h4>
                         <p class="text-center">
-                          A standart features you can get
+                         
                         </p>
                       </div>
-                      <h1 class="price">$100</h1>
+                      <h1 class="price">Gratis</h1>
                       <ul>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Acceso a comunidad médica.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Consultas con Inteligencia Artificial.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Recordatorios de tus citas y medicamentos.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Contactos de emergencia.
                         </li>
                       
                       </ul>
                       <div class="card-footer">
                         <button class="btn btn-primary btn-block">
-                          Order Now
+                          Obtener
                         </button>
                       </div>
                     </div>
@@ -66,43 +62,31 @@
                   <div class="col-md-4 px-0 position-relative z-1">
                     <div class="card card-highlighted shadow-lg">
                       <div class="card-header text-center">
-                        <h4 class="card-title">Enterpreneur</h4>
+                        <h4 class="card-title">Pro</h4>
                         <p></p>
                       </div>
-                      <h1 class="price text-white">$150</h1>
+                      <h1 class="price text-white">$9.99</h1>
                       <ul>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum kolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Acceso a comunidad médica.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Consultas con Inteligencia Artificial.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum kolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Recordatorios de tus citas y medicamentos.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Contactos de emergencia.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum kolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Consultas en Línea.
                         </li>
-                        <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
-                        </li>
-                        <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum kolor
-                          sit amet
-                        </li>
+                      
                       </ul>
                       <div class="card-footer">
                         <button class="btn btn-outline-white btn-block">
-                          Order Now
+                          Obtener
                         </button>
                       </div>
                     </div>
@@ -110,34 +94,34 @@
                   <div class="col-md-4 px-0">
                     <div class="card">
                       <div class="card-header text-center">
-                        <h4 class="card-title">Professional</h4>
+                        <h4 class="card-title">Tokens</h4>
                         <p class="text-center">
-                          A higher features you will need
+                          
                         </p>
                       </div>
-                      <h1 class="price">$100</h1>
+                      <h1 class="price"></h1>
                       <ul>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Acceso a comunidad médica.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Consultas con Inteligencia Artificial.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Recordatorios de tus citas y medicamentos.
                         </li>
                         <li>
-                          <i class="bi bi-check-circle"></i>Lorem ipsum dolor
-                          sit amet
+                          <i class="bi bi-check-circle"></i>Contactos de emergencia.
                         </li>
-                     
+                        <li>
+                          <i class="bi bi-check-circle"></i>Paga consultas por separado.
+                        </li>
+                      
                       </ul>
+                      <br>
                       <div class="card-footer">
                         <button class="btn btn-primary btn-block">
-                          Order Now
+                          Obtener
                         </button>
                       </div>
                     </div>
