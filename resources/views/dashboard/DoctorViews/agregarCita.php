a3a926eb5b0be3b17205e4f48f1ae56a73edfd5d
manuowo2003uwu <Emmrrari@gmail.com>
2023-12-05 22:17:15 -0600

    hice cambios en las vistas del medico

--- resources/views/dashboard/DoctorViews/agregarCita.php
+++ resources/views/dashboard/DoctorViews/agregarCita.php
@@ -1,75 +1,90 @@
 <link rel="stylesheet" href="<?=$utils->assets("assets/extensions/flatpickr/flatpickr.min.css"); ?>" />
+
 <section id="multiple-column-form">
-            <div class="row match-height">
-              <div class="col-9">
-                <div class="card">
-                  <div class="card-header">
-                    <h4 class="card-title">Nueva cita</h4>
-                  </div>
-                  <div class="card-content">
-                    <div class="card-body">
-                      <form class="form">
-                        <div class="row">
-                          <div class="col-md-6 col-12">
-                            <div class="form-group">
-                              <label for="first-name-column">First Name</label>
-                              <input type="text" id="first-name-column" class="form-control" placeholder="First Name" name="fname-column">
-                            </div>
-                          </div>
-                          <div class="col-md-6 col-12">
-                            <div class="form-group">
-                              <label for="last-name-column">Last Name</label>
-                              <input type="text" id="last-name-column" class="form-control" placeholder="Last Name" name="lname-column">
-                            </div>
-                          </div>
-                          <div class="col-md-6 col-12">
-                            <div class="form-group">
-                              <label for="city-column">Fecha de la consulta</label>
-                              <input type="text" class="form-control mb-3 flatpickr-no-config flatpickr-input active" placeholder="Seleccione fecha.." readonly="readonly">
-                            </div>
-                          </div>
-                          <div class="col-md-6 col-12">
-                            <div class="form-group">
-                              <label for="country-floating">Country</label>
-                              <input type="text" id="country-floating" class="form-control" name="country-floating" placeholder="Country">
-                            </div>
-                          </div>
-                          <div class="col-md-6 col-12">
-                            <div class="form-group">
-                              <label for="company-column">Company</label>
-                              <input type="text" id="company-column" class="form-control" name="company-column" placeholder="Company">
-                            </div>
-                          </div>
-                          <div class="col-md-6 col-12">
-                            <div class="form-group">
-                              <label for="email-id-column">Email</label>
-                              <input type="email" id="email-id-column" class="form-control" name="email-id-column" placeholder="Email">
-                            </div>
-                          </div>
-                          <div class="form-group col-12">
-                            <div class="form-check">
-                              <div class="checkbox">
-                                <input type="checkbox" id="checkbox5" class="form-check-input" checked="">
-                                <label for="checkbox5">Remember Me</label>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-12 d-flex justify-content-end">
-                            <button type="submit" class="btn btn-primary me-1 mb-1">
-                              Submit
-                            </button>
-                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">
-                              Reset
-                            </button>
-                          </div>
-                        </div>
-                      </form>
-                    </div>
+  <div class="row match-height">
+    <div class="col-9">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title">Nueva cita</h4>
+        </div>
+        <div class="card-content">
+          <div class="card-body">
+            <form class="form">
+              <div class="row">
+                <div class="col-md-6 col-12">
+                  <div class="form-group">
+                    <label for="patient-name-column">Nombre del Paciente</label>
+                    <input type="text" id="patient-name-column" class="form-control" placeholder="Nombre del Paciente" name="patient-name-column">
                   </div>
                 </div>
+                <div class="col-md-6 col-12">
+                  <div class="form-group">
+                    <label for="patient-last-name-column">Apellido del Paciente</label>
+                    <input type="text" id="patient-last-name-column" class="form-control" placeholder="Apellido del Paciente" name="patient-last-name-column">
+                  </div>
+                </div>
+                <div class="col-md-6 col-12">
+                  <div class="form-group">
+                    <label for="patient-id-column">ID del Paciente</label>
+                    <input type="text" id="patient-id-column" class="form-control" placeholder="ID del Paciente" name="patient-id-column">
+                  </div>
+                </div>
+                <div class="col-md-6 col-12">
+                  <div class="form-group">
+                    <label for="patient-email-column">Numero de telefono del Paciente</label>
+                    <input type="email" id="patient-email-column" class="form-control" name="patient-email-column" placeholder="Numero de telefono del Paciente">
+                  </div>
+                </div>
+                <div class="col-md-6 col-12">
+                  <div class="form-group">
+                    <label for="appointment-date-column">Fecha de la consulta</label>
+                    <input type="text" class="form-control mb-3 flatpickr-no-config flatpickr-input active" placeholder="Seleccione fecha.." readonly="readonly">
+                  </div>
+                </div>
+                <div class="col-md-6 col-12">
+                  <div class="form-group">
+                    <label for="patient-department-column">Departamento del Paciente</label>
+                    <select id="patient-department-column" class="form-control" name="patient-department-column">
+                      <option value="Ahuachapán">Ahuachapán</option>
+                      <option value="Cabañas">Cabañas</option>
+                      <option value="Chalatenango">Chalatenango</option>
+                      <option value="Cuscatlán">Cuscatlán</option>
+                      <option value="La Libertad">La Libertad</option>
+                      <option value="La Paz">La Paz</option>
+                      <option value="La Unión">La Unión</option>
+                      <option value="Morazán">Morazán</option>
+                      <option value="San Miguel">San Miguel</option>
+                      <option value="San Salvador">San Salvador</option>
+                      <option value="San Vicente">San Vicente</option>
+                      <option value="Santa Ana">Santa Ana</option>
+                      <option value="Sonsonate">Sonsonate</option>
+                      <option value="Usulután">Usulután</option>
+                      
+                    </select>
+                  </div>
+                </div>
+                <div class="col-md-12 col-12">
+                  <div class="form-group">
+                    <label for="appointment-reason-column">Motivo de la Consulta</label>
+                    <textarea id="appointment-reason-column" class="form-control" placeholder="Motivo de la Consulta" name="appointment-reason-column"></textarea>
+                  </div>
+                </div>
+                <div class="col-12 d-flex justify-content-end">
+                  <button type="submit" class="btn btn-primary me-1 mb-1">
+                    Enviar
+                  </button>
+                  <button type="reset" class="btn btn-light-secondary me-1 mb-1">
+                    Reiniciar
+                  </button>
+                </div>
               </div>
-            </div>
-          </section>
-          <script src="<?=$utils->assets("assets/extensions/flatpickr/flatpickr.min.js")?>"></script>
-    <script src="<?=$utils->assets("assets/static/js/pages/date-picker.js")?>"></script>
-    
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script src="<?=$utils->assets("assets/extensions/flatpickr/flatpickr.min.js")?>"></script>
+<script src="<?=$utils->assets("assets/static/js/pages/date-picker.js")?>"></script>
--- resources/views/dashboard/DoctorViews/seguimientoCita.php
+++ resources/views/dashboard/DoctorViews/seguimientoCita.php
@@ -1,289 +1,136 @@
 <link rel="stylesheet" href="<?=$utils->assets("assets/extensions/simple-datatables/style.css");?>" />
 <link rel="stylesheet" href="<?=$utils->assets("assets/compiled/css/table-datatable.css");?>" />
 <header class="mb-3">
-        <a href="#" class="burger-btn d-block d-xl-none">
-          <i class="bi bi-justify fs-3"></i>
-        </a>
-      </header>
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
 
-      <div class="page-heading">
-        <div class="page-title">
-          <div class="row">
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
             <div class="col-12 col-md-6 order-md-1 order-last">
-              <h3>Seguimiento de <b>Citas</b></h3>
-              <p class="text-subtitle text-muted">
-               Tu listado de citas de todos tus pacientes
-              </p>
+                <h3>Seguimiento de <b>Citas</b></h3>
+                <p class="text-subtitle text-muted">
+                    Tu listado de citas de todos tus pacientes
+                </p>
             </div>
             <div class="col-12 col-md-6 order-md-2 order-first">
-              <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item">
-                    <a href="<?=$utils->assets("/dashboard");?>">Dashboard</a>
-                  </li>
-                  <li class="breadcrumb-item active" aria-current="page">
-                    Segumiento Cita
-                  </li>
-                </ol>
-              </nav>
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="<?=$utils->assets("/dashboard");?>">Dashboard</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Seguimiento Cita
+                        </li>
+                    </ol>
+                </nav>
             </div>
-          </div>
         </div>
-        <section class="section">
-          <div class="card">
+    </div>
+    <section class="section">
+        <div class="card">
             <div class="card-header">Listado de Pacientes</div>
             <div class="card-body">
-              <table class="table table-striped" id="table1">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>City</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Graiden</td>
-                    <td>vehicula.aliquet@semconsequat.co.uk</td>
-                    <td>076 4820 8838</td>
-                    <td>Offenburg</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Dale</td>
-                    <td>fringilla.euismod.enim@quam.ca</td>
-                    <td>0500 527693</td>
-                    <td>New Quay</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
+                <table class="table table-striped" id="table1">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Motivo de la Consulta</th>
+                            <th>Teléfono</th>
+                            <th>Departamento</th>
+                            <th>Fecha de la Consulta</th>
+                            <th>Estado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                      <td>Isabella</td>
+                      <td>Problemas Respiratorios</td>
+                      <td>(503) 6123-4567</td>
+                      <td>Santa Ana</td>
+                      <td>2023-03-05</td>
+                      <td>
+                          <span class="badge bg-success">Activo</span>
+                      </td>
                   </tr>
                   <tr>
-                    <td>Nathaniel</td>
-                    <td>mi.Duis@diam.edu</td>
-                    <td>(012165) 76278</td>
-                    <td>Grumo Appula</td>
-                    <td>
-                      <span class="badge bg-danger">Inactive</span>
-                    </td>
+                      <td>Mateo</td>
+                      <td>Fractura de Tobillo</td>
+                      <td>(503) 7012-3456</td>
+                      <td>La Paz</td>
+                      <td>2023-03-12</td>
+                      <td>
+                          <span class="badge bg-success">Activo</span>
+                      </td>
                   </tr>
                   <tr>
-                    <td>Darius</td>
-                    <td>velit@nec.com</td>
-                    <td>0309 690 7871</td>
-                    <td>Ways</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
+                      <td>Sofía</td>
+                      <td>Migraña Severa</td>
+                      <td>(503) 5678-9012</td>
+                      <td>Usulután</td>
+                      <td>2023-03-20</td>
+                      <td>
+                          <span class="badge bg-danger">Inactivo</span>
+                      </td>
                   </tr>
                   <tr>
-                    <td>Oleg</td>
-                    <td>rhoncus.id@Aliquamauctorvelit.net</td>
-                    <td>0500 441046</td>
-                    <td>Rossignol</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
+                      <td>Lucas</td>
+                      <td>Herida en la Mano</td>
+                      <td>(503) 2123-4567</td>
+                      <td>Ahuachapán</td>
+                      <td>2023-04-02</td>
+                      <td>
+                          <span class="badge bg-success">Activo</span>
+                      </td>
                   </tr>
                   <tr>
-                    <td>Kermit</td>
-                    <td>diam.Sed.diam@anteVivamusnon.org</td>
-                    <td>(01653) 27844</td>
-                    <td>Patna</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
+                      <td>Emma</td>
+                      <td>Problemas Oculares</td>
+                      <td>(503) 4987-6543</td>
+                      <td>Cabañas</td>
+                      <td>2023-04-15</td>
+                      <td>
+                          <span class="badge bg-danger">Inactivo</span>
+                      </td>
                   </tr>
                   <tr>
-                    <td>Jermaine</td>
-                    <td>sodales@nuncsit.org</td>
-                    <td>0800 528324</td>
-                    <td>Mold</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
+                      <td>Liam</td>
+                      <td>Conmoción Cerebral</td>
+                      <td>(503) 3001-2345</td>
+                      <td>Chalatenango</td>
+                      <td>2023-05-01</td>
+                      <td>
+                          <span class="badge bg-success">Activo</span>
+                      </td>
                   </tr>
                   <tr>
-                    <td>Ferdinand</td>
-                    <td>gravida.molestie@tinciduntadipiscing.org</td>
-                    <td>(016977) 4107</td>
-                    <td>Marlborough</td>
-                    <td>
-                      <span class="badge bg-danger">Inactive</span>
-                    </td>
+                      <td>Ava</td>
+                      <td>Problemas Digestivos</td>
+                      <td>(503) 6543-2109</td>
+                      <td>La Unión</td>
+                      <td>2023-05-10</td>
+                      <td>
+                          <span class="badge bg-danger">Inactivo</span>
+                      </td>
                   </tr>
                   <tr>
-                    <td>Kuame</td>
-                    <td>Quisque.purus@mauris.org</td>
-                    <td>(0151) 561 8896</td>
-                    <td>Tresigallo</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
+                      <td>Noah</td>
+                      <td>Lesión en la Rodilla</td>
+                      <td>(503) 1234-5678</td>
+                      <td>San Vicente</td>
+                      <td>2023-06-02</td>
+                      <td>
+                          <span class="badge bg-success">Activo</span>
+                      </td>
                   </tr>
-                  <tr>
-                    <td>Deacon</td>
-                    <td>Duis.a.mi@sociisnatoquepenatibus.com</td>
-                    <td>07740 599321</td>
-                    <td>Karapınar</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Channing</td>
-                    <td>tempor.bibendum.Donec@ornarelectusante.ca</td>
-                    <td>0845 46 49</td>
-                    <td>Warrnambool</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Aladdin</td>
-                    <td>sem.ut@pellentesqueafacilisis.ca</td>
-                    <td>0800 1111</td>
-                    <td>Bothey</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Cruz</td>
-                    <td>non@quisturpisvitae.ca</td>
-                    <td>07624 944915</td>
-                    <td>Shikarpur</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Keegan</td>
-                    <td>molestie.dapibus@condimentumDonecat.edu</td>
-                    <td>0800 200103</td>
-                    <td>Assen</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Ray</td>
-                    <td>placerat.eget@sagittislobortis.edu</td>
-                    <td>(0112) 896 6829</td>
-                    <td>Hofors</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Maxwell</td>
-                    <td>diam@dapibus.org</td>
-                    <td>0334 836 4028</td>
-                    <td>Thane</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Carter</td>
-                    <td>urna.justo.faucibus@orci.com</td>
-                    <td>07079 826350</td>
-                    <td>Biez</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Stone</td>
-                    <td>velit.Aliquam.nisl@sitametrisus.com</td>
-                    <td>0800 1111</td>
-                    <td>Olivar</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Berk</td>
-                    <td>fringilla.porttitor.vulputate@taciti.edu</td>
-                    <td>(0101) 043 2822</td>
-                    <td>Sanquhar</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Philip</td>
-                    <td>turpis@euenimEtiam.org</td>
-                    <td>0500 571108</td>
-                    <td>Okara</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Kibo</td>
-                    <td>feugiat@urnajustofaucibus.co.uk</td>
-                    <td>07624 682306</td>
-                    <td>La Cisterna</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Bruno</td>
-                    <td>elit.Etiam.laoreet@luctuslobortisClass.edu</td>
-                    <td>07624 869434</td>
-                    <td>Rocca d"Arce</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Leonard</td>
-                    <td>blandit.enim.consequat@mollislectuspede.net</td>
-                    <td>0800 1111</td>
-                    <td>Lobbes</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Hamilton</td>
-                    <td>mauris@diam.org</td>
-                    <td>0800 256 8788</td>
-                    <td>Sanzeno</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Harding</td>
-                    <td>Lorem.ipsum.dolor@etnetuset.com</td>
-                    <td>0800 1111</td>
-                    <td>Obaix</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Emmanuel</td>
-                    <td>eget.lacus.Mauris@feugiatSednec.org</td>
-                    <td>(016977) 8208</td>
-                    <td>Saint-Remy-Geest</td>
-                    <td>
-                      <span class="badge bg-success">Active</span>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+                    </tbody>
+                </table>
             </div>
-          </div>
-        </section>
-      </div>
+        </div>
+    </section>
+</div>
 
-      <script src="<?=$utils->assets('assets/extensions/simple-datatables/umd/simple-datatables.js');?>"></script>
-    <script src="<?=$utils->assets('assets/static/js/pages/simple-datatables.js');?>"></script>
+<script src="<?=$utils->assets('assets/extensions/simple-datatables/umd/simple-datatables.js');?>"></script>
+<script src="<?=$utils->assets('assets/static/js/pages/simple-datatables.js');?>"></script>
