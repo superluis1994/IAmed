6388faca61b470b835e2df6ac50dbea04589d9a5
manuowo2003uwu <Emmrrari@gmail.com>
2023-12-06 14:56:33 -0600

    hice cambios en la vista doctor

--- resources/views/dashboard/DoctorViews/perfilProfesional.php
+++ resources/views/dashboard/DoctorViews/perfilProfesional.php
@@ -1,2 +1,47 @@
-<?php
-echo"Perfil del Profesional de la Salud";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Perfil del Doctor</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <h2>Dr. Juan Pérez</h2>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Información del Doctor</h5>
+            <ul class="list-group">
+                <li class="list-group-item"><strong>Curriculum:</strong> El Dr. Juan Pérez es un reconocido especialista en medicina interna con más de 15 años de experiencia. Ha trabajado en varios hospitales y ha llevado a cabo investigaciones en enfermedades cardíacas.</li>
+                <li class="list-group-item"><strong>Especializaciones:</strong>
+                    <ul>
+                        <li>Medicina Interna</li>
+                        <li>Cardiología</li>
+                        <li>Endocrinología</li>
+                    </ul>
+                </li>
+                <li class="list-group-item"><strong>Clínica:</strong> Clínica Médica San Salvador</li>
+                <li class="list-group-item"><strong>Dirección de la Clínica:</strong> 456 Avenida Principal, Ciudad, El Salvador</li>
+                <li class="list-group-item"><strong>Horarios de Atención:</strong>
+                    <ul>
+                        <li>Lunes a Viernes: 8:00 AM - 5:00 PM</li>
+                        <li>Sábado: 9:00 AM - 1:00 PM</li>
+                    </ul>
+                </li>
+                <li class="list-group-item"><strong>Educación:</strong> Graduado de la Universidad Nacional de El Salvador, Especialización en Medicina Interna en el Hospital Nacional.</li>
+                <li class="list-group-item"><strong>Publicaciones:</strong> Autor de varios artículos científicos en revistas médicas de prestigio.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
