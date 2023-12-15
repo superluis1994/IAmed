21f4e60d2fcd6306c3e68cab39d29baec79d3889
manuowo2003uwu <Emmrrari@gmail.com>
2023-12-15 10:34:47 -0600

    cambios

--- resources/views/dashboard/PacienteViews/agregarRecordatorio.php
+++ resources/views/dashboard/PacienteViews/agregarRecordatorio.php
@@ -1,2 +1,83 @@
-<?php
-echo"Agregar Recordatorio";
+<link rel="stylesheet" href="<?=$utils->assets("assets/extensions/flatpickr/flatpickr.min.css"); ?>" />
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Agregar <b>Recordatorios</b></h3>
+                <p class="text-subtitle text-muted">
+                   puede agregar todos los recordatorios que necesites
+                </p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="<?=$utils->assets("/dashboard");?>">Dashboard</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Agregar Recordatorio
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+<section id="multiple-column-form">
+  <div class="row match-height">
+    <div class="col-9">
+      <div class="card">
+        <div class="card-content">
+          <div class="card-body">
+            <form method="POST" class="form" id="FormRecordatorio" async >
+              <div class="row mt-3">
+                <div class="col-md-6 col-12">
+                <div class="form-group">
+                      <label for="basicInput">Titulo</label>
+                      <input type="text" class="form-control" name="titulo" id="basicInput" placeholder="titulo del recordatorio">
+                    </div>
+                </div>
+                <div class="col-md-6 col-12">
+                  <div class="form-group">
+                    <label for="reminder-date-column">Fecha y hora del Recordatorio</label>
+                    <input type="text" name="fechaHora" class="form-control mb-3 flatpickr-no-config flatpickr-input active" placeholder="Seleccione fecha.." readonly="readonly" id="reminder-date-column">
+                  </div>
+                </div>
+              <div class="col-md-12 col-12">
+                  <div class="form-group">
+                    <label for="appointment-reason-column">Asunto</label>
+                    <textarea id="appointment-reason-column" name="descripcion" class="form-control" placeholder="Asunto del recordatorio" name="appointment-reason-column"></textarea>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-check mb-2">
+                    <input type="checkbox" class="form-check-input" name="recordarme" id="notify-checkbox">
+                    <label class="form-check-label" for="notify-checkbox">Notificarme</label>
+                  </div>
+                </div>
+                <div class="col-12 d-flex justify-content-end">
+                  <button type="submit" class="btn btn-primary me-1 mb-1" onclick="formularioEnvio('<?= $utils->url('/dashboard/recordatorios/add/');?>','FormRecordatorio')">
+                    Guardar
+                  </button>
+                  <button type="reset" class="btn btn-light-secondary me-1 mb-1">
+                    Reiniciar
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+
+<script src="<?=$utils->assets("assets/extensions/flatpickr/flatpickr.min.js")?>"></script>
+<script src="<?=$utils->assets("assets/static/js/pages/date-picker.js")?>"></script>
+<script src="<?=$utils->assets("js/recordatorios/recordatorios.js")?>"></script>
+<script>
+  
+  // function saveReminder() {
+  //   alert("Recordatorio guardado.");
+  // }
+</script>
--- resources/views/dashboard/PacienteViews/historialRecordatorio.php
+++ resources/views/dashboard/PacienteViews/historialRecordatorio.php
@@ -1,2 +1,84 @@
-<?php
-echo"Historial Recordatorio";
+
+<link rel="stylesheet" href="<?=$utils->assets("assets/extensions/simple-datatables/style.css");?>" />
+<link rel="stylesheet" href="<?=$utils->assets("assets/compiled/css/table-datatable.css");?>" />
+<header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+          <i class="bi bi-justify fs-3"></i>
+        </a>
+      </header>
+
+      <div class="page-heading">
+        <div class="page-title">
+          <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+              <h3>Seguimiento de <b>Recordatorios</b></h3>
+              <p class="text-subtitle text-muted">
+               Tu listado de los recordatorios
+              </p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+              <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item">
+                    <a href="<?=$utils->assets("/dashboard");?>">Dashboard</a>
+                  </li>
+                  <li class="breadcrumb-item active" aria-current="page">
+                    Recordatorios
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <section class="section">
+          <div class="card">
+            <div class="card-header">Listado de Recordatorios</div>
+            <div class="card-body">
+              <table class="table table-striped" id="table1">
+                <thead>
+                  <tr>
+                    <th>Titulo</th>
+                    <th>Fecha</th>
+                    <th>Hora</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($data["Recordatorios"] as $key => $value) {
+                    // $active = $key == $data['active'] ? 'active' : '';
+                      $class=[
+                        0=>"bg-danger",
+                        1=>"Realizada"
+                      ];
+                      if($value["completado"]==1){
+                      $class[0]="bg-success";
+                      $class[1]="Pendiente";
+                      
+                    }
+                    echo <<<HTML
+                    <tr>
+                      <td>{$value["titulo"]}</td>
+                      <td>{$value["fecha"]}</td>
+                      <td> {$value["hora"]}</td>
+                      <td>
+                        
+                        <span class="badge {$class[0]}">$class[1]</span>
+                      </td>
+                    </tr>
+              
+            HTML;
+                }
+
+                ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <script src="<?=$utils->assets('assets/extensions/simple-datatables/umd/simple-datatables.js');?>"></script>
+    <script src="<?=$utils->assets('assets/static/js/pages/simple-datatables.js');?>"></script>
+
+
